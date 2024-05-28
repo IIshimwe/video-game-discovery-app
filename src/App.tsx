bcb1143c745711b988fbd8a1eeb1a5085a7d3efb
IIshimwe <ixacson@gmail.com>
2024-05-28 10:57:10 +0200

    Build resposnive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
   return (
     <div>
-      <Button colorScheme="blue">Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" bg="tomato">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area="main" bg="gold">
+          Main
+        </GridItem>
+      </Grid>
     </div>
   );
 };
